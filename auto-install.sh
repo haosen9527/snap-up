2d88c106e76f32992fc0b6036f3fe9a6c7d998be
lvjianghao <ljao1994@163.com>
2019-06-17 12:11:21 +0800

    update

--- auto-install.sh
+++ auto-install.sh
@@ -5,16 +5,17 @@ if [ -f "./google-chrome.deb" ];then
    echo "google-chrome.deb is exits"
 else
   wget -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  ##install
+  dpkg -i google-chrome.deb
 fi
-##install
-sudo dpkg -i google-chrome.deb
-sudo apt-get update
-if [ -f "./chromedriver_linux64.zip"]; then
+#sudo apt-get update
+if [ -f "./chromedriver_linux64.zip"];then
   #statements
+  echo "google-chrome.deb is exits"
 else
-  wget -O chromedriver_linux64.zip http://172.20.216.118/files/207200000031BCB1/npm.taobao.org/mirrors/chromedriver/71.0.3578.137/chromedriver_linux64.zip
+  wget -O chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/75.0.3770.90/chromedriver_linux64.zip
+  unzip ./chromedriver_linux64.zip
 fi
-unzip ./chromedriver_linux64.zip
 
 sudo cp chromedriver /usr/bin
 
